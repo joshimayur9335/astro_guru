991428368ce46f38819d68bc1d1ddcbadabf13a2
joshimayur9335 <joshimayur9335@gmail.com>
2026-07-25 16:57:04 +0530

    Initial Flutter project setup

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF57C00);
+  static const Color secondary = Color(0xFFFFB74D);
+  static const Color background = Color(0xFFFFF8F2);
+  static const Color card = Colors.white;
+  static const Color dark = Color(0xFF1F1F1F);
+  static const Color success = Color(0xFF4CAF50);
+  static const Color gold = Color(0xFFD4AF37);
+}
